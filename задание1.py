28342b09b3ba3497894c2689fd71bcaad16c2592
Sussyamogus2016 <Naratise@gmail.com>
2024-10-29 23:35:09 +0300

    commit1

---
+++ задание1.py
@@ -0,0 +1,22 @@
+while True:
+    try:
+        a = float(input('Введите положительное число a: '))
+        if a > 0:
+            break
+    except:
+        print('Неверно >:(. Попробуйте еще раз')
+
+while True:
+    try:
+        b = float(input('Введите положительное число b, меньшее числа a: '))
+        if (a > b) and (b > 0):
+            break
+    except:
+        print('Неверно >:(. Попробуйте еще раз')
+while a >= b:
+    a = a-b
+print('Незанятая часть a:', a)
+
+
+
+
