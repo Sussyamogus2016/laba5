bb9d97da27772ebd03a5b2d780f0b40c259cac09
Sussyamogus2016 <Naratise@gmail.com>
2024-10-30 08:35:02 +0300

    commit 5

--- задание3.py
+++ задание3.py
@@ -1,6 +1,10 @@
 import math
-a = (2*math.pi)/6
+a = (math.pi)/3
 v = 35
 g = 9.81
-x0 = (math.tan(a)*2*v**2*math.tan(a))/g
-for i in range()
+x0 = (math.tan(a)*2*v**2*math.cos(a)**2)/g
+
+x0 = round(x0)
+for x in range(100, 10000, 1):
+    y = x * math.tan(a) + ((g*x**2)/(2*v**2*math.cos(a)**2))
+    print(y/100)
